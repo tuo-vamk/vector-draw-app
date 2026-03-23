a6f50855dafdf891100e1444d12e7961c8e64369
Matti <git@jakomail.com>
2026-03-23 14:07:43 +0200

    change getLength to calculateLength

--- src/main/java/com/vectordraw/Line.java
+++ src/main/java/com/vectordraw/Line.java
@@ -12,7 +12,7 @@ public class Line extends Shape {
             this.end = end;
         }
 
-    public double getLength() {
+    public double calculateLength() {
         int dx = end.x - start.x;
         int dy = end.y - start.y;
         return Math.sqrt(dx * dx + dy * dy);
